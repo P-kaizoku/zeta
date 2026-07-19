351edb9ca8ec0122dfce7efcdeae85d5091acff3
pabitra <pabitram322@gmail.com>
2026-07-19 10:13:01 +0530

    implement client

--- internal/transport/ws/client.go
+++ internal/transport/ws/client.go
@@ -1,6 +1,9 @@
 package ws
 
 import (
+	"time"
+
+	"github.com/P-kaizoku/zeta/internal/domain"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
@@ -11,4 +14,31 @@ type Client struct {
 	send    chan []byte
 	userID  uuid.UUID
 	roomIDs map[uuid.UUID]bool
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+	for message := range c.send {
+		c.conn.WriteMessage(websocket.TextMessage, message)
+	}
+}
+
+func (c *Client) readPump() {
+	defer c.conn.Close()
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			c.hub.unregister <- c
+			break
+		}
+		payload := &domain.Message{
+			ID:        uuid.New(),
+			RoomID:    c.roomID,
+			SenderID:  c.userID,
+			Content:   string(msg),
+			CreatedAt: time.Now(),
+		}
+		c.hub.broadcast <- payload
+
+	}
 }
