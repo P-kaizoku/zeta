c411875bf6a664e544c6b38052867c6092d74ec1
pabitra <pabitram322@gmail.com>
2026-07-19 10:02:17 +0530

    implement hub

---
+++ internal/transport/ws/hub.go
@@ -0,0 +1,44 @@
+package ws
+
+import (
+	"encoding/json"
+
+	"github.com/P-kaizoku/zeta/internal/domain"
+)
+
+type Hub struct {
+	clients    map[*Client]bool
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan *domain.Message
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			payload, err := json.Marshal(message)
+			if err != nil {
+				continue
+			}
+
+			for client := range h.clients {
+				if client.roomIDs[message.RoomID] {
+					select {
+					case client.send <- payload:
+					default:
+						close(client.send)
+						delete(h.clients, client)
+					}
+				}
+			}
+		}
+	}
+}
